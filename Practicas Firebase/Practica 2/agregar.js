100ce839c55fc7ce845ad0536aa6b4a703c88d8a
Salomón Romero <53873069+fenix0499@users.noreply.github.com>
2021-05-04 17:13:41 -0500

    Add files via upload

---
+++ Practicas Firebase/Practica 2/agregar.js
@@ -0,0 +1,12 @@
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  var registro = new Registro(
+    null,
+    formulario.nombre.value,
+    formulario.codigo.value
+  );
+  registro.agregar();
+  formulario.nombre.value = "";
+  formulario.codigo.value = "";
+  $("#ventananuevo").modal("hide");
+});
